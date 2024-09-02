a51f28aa746f84cff284ac76fd3ba89d8f0ae747
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2024-09-03 03:59:17 +0530

    uploading SEM 3 OOPS project

---
+++ Rubiks_solver-main/Model/RubiksCube3dArray.cpp
@@ -0,0 +1,290 @@
+#include "RubiksCube.h"
+
+class RubiksCube3dArray : public RubiksCube {
+private:
+
+    void rotateFace(int ind) {
+        char temp_arr[3][3] = {};
+        // * getting the face in another 2d matrix
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                temp_arr[i][j] = cube[ind][i][j];
+            }
+        }
+        //* applying the transformation on the face
+        for (int i = 0; i < 3; i++) cube[ind][0][i] = temp_arr[2 - i][0];
+        for (int i = 0; i < 3; i++) cube[ind][i][2] = temp_arr[0][i];
+        for (int i = 0; i < 3; i++) cube[ind][2][2 - i] = temp_arr[i][2];
+        for (int i = 0; i < 3; i++) cube[ind][2 - i][0] = temp_arr[2][2 - i];
+    }
+
+public:
+    char cube[6][3][3]{};
+
+    /*
+    * The constructor called to make the 3d rubiks cube 
+    */
+   
+    RubiksCube3dArray() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++)
+                    cube[i][j][k] = getColorLetter(COLOR(i));
+            }
+        }
+    }
+
+    /*
+        * override the virtual function in the abstract class 
+    */
+
+    COLOR getColor(FACE face, unsigned row, unsigned col) const override {
+        char color = cube[int(face)][row][col];
+        switch (color) {
+            case 'B':
+                return COLOR::BLUE;
+            case 'R':
+                return COLOR::RED;
+            case 'G':
+                return COLOR::GREEN;
+            case 'O':
+                return COLOR::ORANGE;
+            case 'Y':
+                return COLOR::YELLOW;
+            default:
+                return COLOR::WHITE;
+        }
+    }
+
+    /*
+        * to check if the cube issolved or not
+    */
+    bool isSolved() const override {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (this->cube[i][j][k] == getColorLetter(COLOR(i))) continue;
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+        * Declaring the corresponting resultant state for each move in 3d representation
+    */
+    RubiksCube &u() override {
+        // *  up move so only the top face will be rotated and its idex value = 0
+        this->rotateFace(0);
+
+        char temp_arr[3] = {}; //* To store the edge piece that shares edge with the top face
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[4][0][2 - i]; // * top row left face
+        for (int i = 0; i < 3; i++) cube[4][0][2 - i] = cube[1][0][2 - i];
+        for (int i = 0; i < 3; i++) cube[1][0][2 - i] = cube[2][0][2 - i];
+        for (int i = 0; i < 3; i++) cube[2][0][2 - i] = cube[3][0][2 - i];
+        for (int i = 0; i < 3; i++) cube[3][0][2 - i] = temp_arr[i];
+
+        return *this; // * return the updates cube
+    }
+
+    RubiksCube &uPrime() override {
+        // * 3 clockwise  =  1 anti-clockwise
+
+        this->u();
+        this->u();
+        this->u();
+
+        return *this;
+    }
+
+    RubiksCube &u2() override {
+        //
+        this->u();
+        this->u();
+
+        return *this;
+    }
+
+    RubiksCube &l() override {
+        this->rotateFace(1);
+
+        char temp_arr[3] = {};
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[0][i][0];
+        for (int i = 0; i < 3; i++) cube[0][i][0] = cube[4][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[4][2 - i][2] = cube[5][i][0];
+        for (int i = 0; i < 3; i++) cube[5][i][0] = cube[2][i][0];
+        for (int i = 0; i < 3; i++) cube[2][i][0] = temp_arr[i];
+
+        return *this;
+    }
+
+    RubiksCube &lPrime() override {
+        this->l();
+        this->l();
+        this->l();
+
+        return *this;
+    }
+
+    RubiksCube &l2() override {
+        this->l();
+        this->l();
+
+        return *this;
+    }
+
+    RubiksCube &f() override {
+        this->rotateFace(2);
+
+        char temp_arr[3] = {};
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[0][2][i];
+        for (int i = 0; i < 3; i++) cube[0][2][i] = cube[1][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[1][2 - i][2] = cube[5][0][2 - i];
+        for (int i = 0; i < 3; i++) cube[5][0][2 - i] = cube[3][i][0];
+        for (int i = 0; i < 3; i++) cube[3][i][0] = temp_arr[i];
+
+        return *this;
+    }
+
+    RubiksCube &fPrime() override {
+        this->f();
+        this->f();
+        this->f();
+
+        return *this;
+    }
+
+    RubiksCube &f2() override {
+        this->f();
+        this->f();
+
+        return *this;
+    }
+
+    RubiksCube &r() override {
+        this->rotateFace(3);
+
+        char temp_arr[3] = {};
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[0][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[0][2 - i][2] = cube[2][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[2][2 - i][2] = cube[5][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[5][2 - i][2] = cube[4][i][0];
+        for (int i = 0; i < 3; i++) cube[4][i][0] = temp_arr[i];
+
+        return *this;
+    }
+
+    RubiksCube &rPrime() override {
+        this->r();
+        this->r();
+        this->r();
+
+        return *this;
+    }
+
+    RubiksCube &r2() override {
+        this->r();
+        this->r();
+
+        return *this;
+    }
+
+    RubiksCube &b() override {
+        this->rotateFace(4);
+
+        char temp_arr[3] = {};
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[0][0][2 - i];
+        for (int i = 0; i < 3; i++) cube[0][0][2 - i] = cube[3][2 - i][2];
+        for (int i = 0; i < 3; i++) cube[3][2 - i][2] = cube[5][2][i];
+        for (int i = 0; i < 3; i++) cube[5][2][i] = cube[1][i][0];
+        for (int i = 0; i < 3; i++) cube[1][i][0] = temp_arr[i];
+
+        return *this;
+    }
+
+    RubiksCube &bPrime() override {
+        this->b();
+        this->b();
+        this->b();
+
+        return *this;
+    }
+
+    RubiksCube &b2() override {
+        this->b();
+        this->b();
+
+        return *this;
+    }
+
+    RubiksCube &d() override {
+        this->rotateFace(5);
+
+        char temp_arr[3] = {};
+        for (int i = 0; i < 3; i++) temp_arr[i] = cube[2][2][i];
+        for (int i = 0; i < 3; i++) cube[2][2][i] = cube[1][2][i];
+        for (int i = 0; i < 3; i++) cube[1][2][i] = cube[4][2][i];
+        for (int i = 0; i < 3; i++) cube[4][2][i] = cube[3][2][i];
+        for (int i = 0; i < 3; i++) cube[3][2][i] = temp_arr[i];
+
+        return *this;
+    }
+
+    RubiksCube &dPrime() override {
+        this->d();
+        this->d();
+        this->d();
+
+        return *this;
+    }
+
+    RubiksCube &d2() override {
+        this->d();
+        this->d();
+
+        return *this;
+    }
+
+    // * oveloading the == operator -> use in mapping
+    bool operator==(const RubiksCube3dArray &r1) const {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (r1.cube[i][j][k] != cube[i][j][k]) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // * oveloading the = operator -> to set the value
+    RubiksCube3dArray &operator=(const RubiksCube3dArray &r1) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    cube[i][j][k] = r1.cube[i][j][k];
+                }
+            }
+        }
+        return *this;
+    }
+};
+
+struct Hash3d {
+
+    //* make a hashing function cube -> string -> hash using stl
+    // * size_t ensures that the output can be fit in it
+    size_t operator()(const RubiksCube3dArray &r1) const {
+        string str = "";
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    str += r1.cube[i][j][k];
+                }
+            }
+        }
+        return hash<string>()(str); // * -> returns a hash of the string for used in map
+    }
+
+};
